10008c09ebfa772660bb551f18e8ae2df0444afd
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-03-31 19:36:37 -0300

    Adicionado mensagem de fim de jogo(acerto)

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -1,15 +1,45 @@
 ﻿string palavraSecreta = "ABACATE";
 
+char[] letrasCorretas = new char[7];
+
+for (int contadorLetras = 0; contadorLetras < 7; contadorLetras++)
+{
+    letrasCorretas[contadorLetras] = '_';
+}
+
 bool jogadorAcertou = false;
 
 while (!jogadorAcertou)
 {
-    Console.Clear();
-    Console.WriteLine("Jogo da Forca");
-    Console.Write("Digite uma letra: ");
-    char letra = Convert.ToChar(Console.ReadLine());
+    //Console.Clear();
 
-    Console.WriteLine(letra);
-    Console.ReadLine();
+    for (int contadorLetras = 0; contadorLetras < 7; contadorLetras++)
+    {
+        Console.Write(letrasCorretas[contadorLetras]);
+    }
+
+    Console.Write("\nDigite uma letra: ");
+    char chute = Convert.ToChar(Console.ReadLine());
+
+    for (int contadorPalavraSecreta = 0; contadorPalavraSecreta < palavraSecreta.Length; contadorPalavraSecreta++)
+    {
+        char letraSecretaAtual = palavraSecreta[contadorPalavraSecreta];
+
+        if (chute == letraSecretaAtual)
+        {
+            letrasCorretas[contadorPalavraSecreta] = chute;
+        }
+    }
+
+    string letrasCorretasCompleta = string.Join("", letrasCorretas);
+
+    if (palavraSecreta == letrasCorretasCompleta)
+    {
+        jogadorAcertou = true;
+        Console.WriteLine($"Parabens você acertou! A palavra secreta era: {palavraSecreta}");
+    }
+
 }
 
+Console.WriteLine("Digite ENTER para sair...");
+Console.ReadLine();
